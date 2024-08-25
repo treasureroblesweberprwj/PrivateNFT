11d6ab639de75d730e0c3438b6e3db648572e4f5
treasureroblesweberprwj <treasureroblesweberprwj@hotmail.com>
2024-08-25 06:08:16 +0000

    Initialised

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("kiuo","")
+        ERC721("","uioo")
         Ownable(initialOwner)
     {}
 
